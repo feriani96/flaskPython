aa853ed49285cc12d92e7cfb09f668d7b304b331
feriani96 <ferianiyasmine3@gmail.com>
2023-06-01 14:11:03 +0200

    the usage of Blueprints

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
-from flask import Flask, request
+from flask import Flask
+from blueprints.basic_endpoints import blueprint as basic_endpoints
 
 app = Flask(__name__)
+app.register_blueprint(basic_endpoints)
 
-@app.route('/basic_api/hello_world')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/basic_api/entities', methods=['GET', 'POST'])
-def entities():
-    if request.method == "GET":
-        return {
-            'message': 'This endpoint should return a list of entities',
-            'method': request.method
-        }
-    if request.method == "POST":
-        return {
-            'message': 'This endpoint should create an entity',
-            'method': request.method,
-		'body': request.json
-        }
-
-@app.route('/basic_api/entities/<int:entity_id>', methods=['GET', 'PUT', 'DELETE'])
-def entity(entity_id):
-    if request.method == "GET":
-        return {
-            'id': entity_id,
-            'message': 'This endpoint should return the entity {} details'.format(entity_id),
-            'method': request.method
-        }
-    if request.method == "PUT":
-        return {
-            'id': entity_id,
-            'message': 'This endpoint should update the entity {}'.format(entity_id),
-            'method': request.method,
-		'body': request.json
-        }
-    if request.method == "DELETE":
-        return {
-            'id': entity_id,
-            'message': 'This endpoint should delete the entity {}'.format(entity_id),
-            'method': request.method
-        }
+if __name__ == "__main__":
+    app.run()
